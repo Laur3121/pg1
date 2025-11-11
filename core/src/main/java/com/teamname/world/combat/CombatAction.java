2958bfe2712d39a66d46c9231112b110e5070552
Konitann <m20046@g.metro-cit.ac.jp>
2025-11-11 14:23:33 +0900

    battle branch initial commit

---
+++ core/src/main/java/com/teamname/world/combat/CombatAction.java
@@ -0,0 +1,12 @@
+package com.teamname.world.combat;
+
+/**
+ * 戦闘で実行可能なアクション
+ */
+public enum CombatAction {
+    ATTACK,      // 通常攻撃
+    DEFEND,      // 防御
+    SKILL,       // スキル使用
+    ITEM,        // アイテム使用
+    FLEE         // 逃走
+}
