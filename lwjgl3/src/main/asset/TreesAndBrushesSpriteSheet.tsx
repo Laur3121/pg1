34d8c19139edb68d143b41128a5584da80291f6f
Laur3121 <2f4f4f.rgb@gmail.com>
2025-11-25 16:12:46 +0900

    tmxとアセット追加

---
+++ lwjgl3/src/main/asset/TreesAndBrushesSpriteSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TreesAndBrushesSpriteSheet" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="../Documents/assets/Free/TreesAndBrushes/TreesAndBrushesSpriteSheet.png" width="278" height="178"/>
+</tileset>
